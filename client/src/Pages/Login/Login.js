f7fc73a4b1dda36f74ffaaf04758a3fc14830546
dayrell19 <leonardodayrell.reis@gmail.com>
2021-08-16 12:15:36 -0700

    Login styling done

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,16 +1,61 @@
 import React from "react";
 import "./Login.css";
+import { useHistory } from "react-router-dom";
+// import { useState } from "react";
+// import Axios from "axios";
 
 const Login = () => {
+  let history = useHistory();
   return (
     <div className="loginContainer">
       <div className="loginContainerTop">
-        <div className="loginContainerLeft"></div>
+        <div className="loginContainerLeft">
+          <h1 className="leftHeader">DatingApp.com</h1>
+        </div>
         <div className="loginContainerRight">
-          <div className="inputsContainer">LOGIN PAGE</div>
+          <div className="inputsContainer">
+            <h1 className="loginHeader">Login Here!</h1>
+            <h2 className="loginSubheader">The best dating website!</h2>
+
+            <div className="individualLoginInput">
+              <input
+                className="loginInput"
+                type="text"
+                placeholder="Username"
+                name="Username"
+                id="username"
+                required
+              />
+              <label className="loginLabel">Username</label>
+            </div>
+
+            <div className="individualLoginInput">
+              <input
+                className="loginInput"
+                type="password"
+                placeholder="Password"
+                name="Password"
+                id="password"
+                required
+              />
+              <label className="loginLabel">Password</label>
+            </div>
+
+            <button className="loginButton">Login</button>
+            <p
+              className="registerLink"
+              onClick={() => {
+                history.push("/register");
+              }}
+            >
+              Don't have an account? Register here!
+            </p>
+          </div>
         </div>
       </div>
-      <div className="loginContainerBottom"></div>
+      <div className="loginContainerBottom">
+        <h1 className="bottomHeader">Live your life to the fullest!</h1>
+      </div>
     </div>
   );
 };
