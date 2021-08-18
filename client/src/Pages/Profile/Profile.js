db3fdab1631972770f93c80cfc894eb95f289fab
dayrell19 <leonardodayrell.reis@gmail.com>
2021-08-18 11:03:13 -0700

    Styling of Profile Page done

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -18,6 +18,7 @@ const USER_DATA_DEFAULT = {
 
 const Profile = () => {
   const [userData, setUserData] = useState(USER_DATA_DEFAULT);
+  const [userEdited, setUserEdited] = useState(USER_DATA_DEFAULT);
   const [editData, setEditData] = useState(false);
   const [deleteUser, setDeleteUser] = useState(false);
 
@@ -29,67 +30,210 @@ const Profile = () => {
     });
   }, []);
 
+  const updateUser = () => {
+    console.log(userEdited);
+  };
+
   return (
     <>
       <Navbar />
       <div className="profileContainer">
-        <div className="profileDataContainer">
-          <h1 className="profileHeader">Your Profile</h1>
-          <div className="infoContainer">
-            <h3 className="profileLabel">Name:</h3>
-            <h3 className="profileData">{userData.firstName}</h3>
-            <h3 className="profileData">{userData.lastName}</h3>
-          </div>
-
-          <div className="infoContainer">
-            <h3 className="profileLabel">Age:</h3>
-            <h3 className="profileData">{userData.age}</h3>
-          </div>
-
-          <div className="infoContainer">
-            <h3 className="profileLabel">Gender:</h3>
-            <h3 className="profileData">{userData.gender}</h3>
-          </div>
-
-          <div className="infoContainer">
-            <h3 className="profileLabel">Email:</h3>
-            <h3 className="profileData">{userData.email}</h3>
-          </div>
-
-          <div className="infoContainer">
-            <h3 className="profileLabel">Username:</h3>
-            <h3 className="profileData">{userData.username}</h3>
-          </div>
-
-          <div className="passwordContainer">
-            <div className="passwordLeft">
-              <h3 className="profileLabel">Password:</h3>
-              <h3 className="profileData">Secured</h3>
+        {editData ? (
+          <div className="profileDataContainer">
+            <h1 className="profileEditHeader">Your Profile</h1>
+            <div className="infoEditContainer">
+              <h3 className="profileLabel">Name:</h3>
+              <input
+                className="profileDataInput"
+                placeholder={userData.firstName}
+                type="text"
+                onChange={(event) => {
+                  setUserEdited((currentData) => ({
+                    ...currentData,
+                    firstName: event.target.value,
+                  }));
+                }}
+              />
+              <input
+                className="profileDataInput"
+                placeholder={userData.lastName}
+                type="text"
+                onChange={(event) => {
+                  setUserEdited((currentData) => ({
+                    ...currentData,
+                    lastName: event.target.value,
+                  }));
+                }}
+              />
             </div>
-            <div className="passwordRight">
-              <button className="changePasswordButton">Change Password</button>
+
+            <div className="infoEditContainer">
+              <h3 className="profileLabel">Age:</h3>
+              <input
+                className="profileDataInput"
+                placeholder={userData.age}
+                type="number"
+                onChange={(event) => {
+                  setUserEdited((currentData) => ({
+                    ...currentData,
+                    age: event.target.value,
+                  }));
+                }}
+              />
+            </div>
+
+            <div className="editGenderRegisterInput">
+              <h3 className="profileLabel">Gender:</h3>
+              <label for="femaleOP" className="editGenderLabel">
+                <input
+                  type="radio"
+                  id="femaleOP"
+                  name="selector"
+                  tabIndex="1"
+                  onClick={() => {
+                    setUserEdited((currentData) => ({
+                      ...currentData,
+                      gender: "female",
+                    }));
+                  }}
+                />
+                <span>Female</span>
+              </label>
+
+              <label for="maleOP" className="editGenderLabel">
+                <input
+                  type="radio"
+                  id="maleOP"
+                  name="selector"
+                  tabIndex="2"
+                  onClick={() => {
+                    setUserEdited((currentData) => ({
+                      ...currentData,
+                      gender: "male",
+                    }));
+                  }}
+                />
+                <span>Male</span>
+              </label>
+
+              <label for="otherOP" className="editGenderLabel">
+                <input
+                  type="radio"
+                  id="otherOP"
+                  name="selector"
+                  tabIndex="3"
+                  onClick={() => {
+                    setUserEdited((currentData) => ({
+                      ...currentData,
+                      gender: "other",
+                    }));
+                  }}
+                />
+                <span>Other</span>
+              </label>
+            </div>
+
+            <div className="infoEditContainer">
+              <h3 className="profileLabel">Email:</h3>
+              <input
+                className="profileDataInput"
+                placeholder={userData.email}
+                type="text"
+                onChange={(event) => {
+                  setUserEdited((currentData) => ({
+                    ...currentData,
+                    email: event.target.value,
+                  }));
+                }}
+              />
+            </div>
+
+            <div className="infoEditContainer">
+              <h3 className="profileLabel">Username:</h3>
+              <input
+                className="profileDataInput"
+                placeholder={userData.username}
+                type="text"
+                onChange={(event) => {
+                  setUserEdited((currentData) => ({
+                    ...currentData,
+                    username: event.target.value,
+                  }));
+                }}
+              />
+            </div>
+
+            <h3 className="editProfileBottomLabel">
+              The password cannot be eddited in this page
+            </h3>
+
+            <div className="profileButtonsBottom">
+              <button className="updateDataButton" onClick={updateUser}>
+                Update
+              </button>
             </div>
           </div>
+        ) : (
+          <div className="profileDataContainer">
+            <h1 className="profileHeader">Your Profile</h1>
+            <div className="infoContainer">
+              <h3 className="profileLabel">Name:</h3>
+              <h3 className="profileData">{userData.firstName}</h3>
+              <h3 className="profileData">{userData.lastName}</h3>
+            </div>
 
-          <div className="profileButtonsBottom">
-            <button
-              className="editDataButton"
-              onClick={() => {
-                setEditData(true);
-              }}
-            >
-              Edit your info
-            </button>
-            <button
-              className="deleteProfileButton"
-              onClick={() => {
-                setDeleteUser(true);
-              }}
-            >
-              Delete Profile
-            </button>
+            <div className="infoContainer">
+              <h3 className="profileLabel">Age:</h3>
+              <h3 className="profileData">{userData.age}</h3>
+            </div>
+
+            <div className="infoContainer">
+              <h3 className="profileLabel">Gender:</h3>
+              <h3 className="profileData">{userData.gender}</h3>
+            </div>
+
+            <div className="infoContainer">
+              <h3 className="profileLabel">Email:</h3>
+              <h3 className="profileData">{userData.email}</h3>
+            </div>
+
+            <div className="infoContainer">
+              <h3 className="profileLabel">Username:</h3>
+              <h3 className="profileData">{userData.username}</h3>
+            </div>
+
+            <div className="passwordContainer">
+              <div className="passwordLeft">
+                <h3 className="profileLabel">Password:</h3>
+                <h3 className="profileData">Secured</h3>
+              </div>
+              <div className="passwordRight">
+                <button className="changePasswordButton">
+                  Change Password
+                </button>
+              </div>
+            </div>
+
+            <div className="profileButtonsBottom">
+              <button
+                className="editDataButton"
+                onClick={() => {
+                  setEditData(true);
+                }}
+              >
+                Edit your info
+              </button>
+              <button
+                className="deleteProfileButton"
+                onClick={() => {
+                  setDeleteUser(true);
+                }}
+              >
+                Delete Profile
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       {deleteUser && <DeleteUser setDeleteUser={setDeleteUser} />}
     </>
